ce59df0e282463f3abe48b0c44f931de20de4d0f
eya hidri <hidri.eya4@gmail.com>
2019-05-11 16:00:17 +0200

    First Commit

---
+++ src/main/java/springBoot/Application.java
@@ -0,0 +1,16 @@
+package springBoot;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+
+    }
+
+
+}
---
+++ src/main/java/springBoot/controller/ControllerApplication.java
@@ -0,0 +1,14 @@
+package springBoot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerApplication {
+
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String homePage(){
+        return "Hello world";
+    }
+}
